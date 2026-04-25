0660d1f084eb1cb8545bb10926945f9b6794d73d
guilherme-makerspace <guilhermeoliveiraaraujo1603@gmail.com>
2026-04-25 09:18:20 -0300

    fix: corrige private key no vercel

--- app/api/dialogflow/route.js
+++ app/api/dialogflow/route.js
@@ -1,9 +1,13 @@
 import { SessionsClient } from '@google-cloud/dialogflow'
 
+const privateKey = process.env.GOOGLE_PRIVATE_KEY
+  ?.replace(/\\n/g, '\n')
+  ?.replace(/^"|"$/g, '')
+
 const client = new SessionsClient({
   credentials: {
     client_email: process.env.GOOGLE_CLIENT_EMAIL,
-    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    private_key: privateKey,
   }
 })
 
